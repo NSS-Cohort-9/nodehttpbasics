d241e076f0ae91c91564f2152e9a9ca640e882f4
Scott Humphries <scott@sscotth.io>
2015-07-29 23:24:00 -0500

    add PORT env variable

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
-require('./lib/server')(1337);
+var port = process.env.PORT || 1337;
+require('./lib/server')(port);
 
-console.log('Server running on http://localhost:1337');
+console.log('Server running on http://localhost:' + port);
